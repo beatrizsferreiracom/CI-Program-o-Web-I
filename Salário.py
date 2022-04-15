228cd4a0c84fdb3362d94873dafddd94d0526dde
beatrizsferreiracom <102437407+beatrizsferreiracom@users.noreply.github.com>
2022-04-15 14:09:33 -0300

    Add files via upload

---
+++ Salário.py
@@ -0,0 +1,9 @@
+#Crie um programa em Python que leia o primeiro nome de uma pessoa e seu salário no mês de abril.
+
+nome = input("Nome: ")
+
+salario = input("Salário do mês de abril: ")
+
+informaçoes = "O salário de" + " " + nome + " " + "no mês de abril foi de R$"  + salario + ",00 reais"
+
+print(informaçoes)
